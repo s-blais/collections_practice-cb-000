d0bbba846741853316c95140f8f7253e7d2d7b21
Stephen Blais <stephen@s-blais.com>
2019-08-22 16:40:49 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -13,9 +13,6 @@ def sort_array_char_count (array)
 end
 
 def swap_elements (array)
-  array.collect do |a,b|
-    if a.index == 1 && b.index == 2
-      reverse
-    end
-  end
+  array[1], array[2] = array[2], array[1]
+  array
 end
