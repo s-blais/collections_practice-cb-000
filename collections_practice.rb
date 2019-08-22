e9a0b20293ccd415985da3906cfc04bdcc4000bb
Stephen Blais <stephen@s-blais.com>
2019-08-22 16:25:38 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -4,5 +4,17 @@ def sort_array_asc (array)
 end
 
 def sort_array_desc (array)
-  array.sort.reverse
+  array.sort {|a,b| b <=> a}
+  # array.sort.reverse also works!
 end
+
+def sort_array_char_count (array)
+  array.sort {|a,b| a.length <=> b.length}
+end
+
+def swap_elements (array)
+  array.collect do |a,b|
+    if a.index == 1 && b.index == 2
+      a.index = 2
+      b.index = 1
+    end
